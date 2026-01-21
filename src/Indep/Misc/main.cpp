764b784b85b9c93d2a5779ef1cffbc6625422d92
Dorian Pazur <c00305750@setu.ie>
2026-01-21 13:41:35 +0000

    prepare to get GX working, just clears screen (also verifies that physics do somewhat work)

--- src/Indep/Misc/main.cpp
+++ src/Indep/Misc/main.cpp
@@ -1,83 +1,32 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <cstdio>
+#include <cstdint>
+#include <cstdlib>
 #include <string.h>
 #include <malloc.h>
 #include <ogcsys.h>
 #include <gccore.h>
+#include <gcmodplay.h>
 
 #include <tWare/Time.h>
 
 #include "includetest.h"
 #include "btBulletDynamicsCommon.h"
 
-static void *xfb = NULL;
+
+static uint8_t currentBuffer = 0;
+static void *xfb[2] = { NULL, NULL}; // double buffered
 static GXRModeObj *rmode = NULL;
 
-void *Initialise();
+f32 yscale;
+u32 xfbHeight;
+GXColor background = {0xFF, 0x40, 0xFF, 0xff};
+Mtx44 v,p; // view and perspective matrices
 
-void* alignedMallocLog(size_t size, int alignment)
-{	
-    size += alignment - (size % alignment);
-	
-	printf("Allocating %u aligned bytes\n", size);
-	for (int i = 0; i < 30; i++)
-	{
-		VIDEO_WaitVSync();
-	}
-	void* memory = malloc(size);
-	if (!memory)
-	{
-		printf("Allocating %u aligned bytes failed!\n", size);
-		for (int i = 0; i < 60; i++)
-		{
-			VIDEO_WaitVSync();
-		}
-	}
-	else
-	{
-		printf("Succeeded in allocating %u aligned bytes!\n", size);
-	}
-	return memory;
-}	
-
-void alignedFreeLog(void* block)
-{
-	printf("Freeing aligned memory.\n");
-	free(block);
-}
-
-void* mallocLog(size_t size)
-{
-	printf("Allocating %u bytes\n", size);
-	for (int i = 0; i < 30; i++)
-	{
-		VIDEO_WaitVSync();
-	}
-	void* memory = malloc(size);
-	if (!memory)
-	{
-		printf("Allocating %u bytes failed!\n", size);
-		for (int i = 0; i < 240; i++)
-		{
-			VIDEO_WaitVSync();
-		}
-	}
-	else
-	{
-		printf("Succeeded in allocating %u bytes!\n", size);
-	}
-	return memory;
-}	
-
-void freeLog(void* block)
-{
-	printf("Freeing memory.\n");
-	free(block);
-}
+void Initialise();
 
 int main(int argc, char **argv) {
 
-	xfb = Initialise();
+	Initialise();
 
 	printf("\nHello World!\n");
 	printf("%s\n", szTestString);
@@ -201,18 +150,59 @@ int main(int argc, char **argv) {
 	float fps = 0.0f;
 	
 	///-----stepsimulation_start-----
-	for (i = 0; i < 150; i++)
-	{
+	//for (i = 0; i < 150; i++)
+	//{
+	//	unsigned int now = tGetTicker();
+	//	float frameTime = tGetTickerDifference(prevFrameTime, now);
+	//	prevFrameTime = now;
+	//	
+	//	fps = 1.0f / (frameTime * 0.1f);
+	//	if (i > 75)
+	//		avgfps += fps;
+	//	
+	//	dynamicsWorld->stepSimulation(1.f / 60.f, 2);
+	//	
+	//	//print positions of all objects
+	//	for (int j = dynamicsWorld->getNumCollisionObjects() - 1; j >= 0; j--)
+	//	{
+	//		btCollisionObject* obj = dynamicsWorld->getCollisionObjectArray()[j];
+	//		btRigidBody* body = btRigidBody::upcast(obj);
+	//		btTransform trans;
+	//		if (body && body->getMotionState())
+	//		{
+	//			body->getMotionState()->getWorldTransform(trans);
+	//		}
+	//		else
+	//		{
+	//			trans = obj->getWorldTransform();
+	//		}
+	//		//printf("world pos object %d = %f,%f,%f\n", j, float(trans.getOrigin().getX()), float(trans.getOrigin().getY()), float(trans.getOrigin().getZ()));
+	//	}
+	//}
+	//
+	//avgfps /= 75.0f;
+	//
+	//printf("average fps during latter half of physics sim = %.2f\n", avgfps);
+	//printf("last fps value = %.2f\n", fps);
+	
+	///-----stepsimulation_end-----
+	
+	//cleanup in the reverse order of creation/initialization
+
+	prevFrameTime = tGetTicker();
+
+	while(1) {
 		unsigned int now = tGetTicker();
 		float frameTime = tGetTickerDifference(prevFrameTime, now);
 		prevFrameTime = now;
 		
-		fps = 1.0f / (frameTime * 0.1f);
-		if (i > 75)
-			avgfps += fps;
+		VIDEO_WaitVSync();
+		currentBuffer ^= 1;
+		PAD_ScanPads();
 		
-		dynamicsWorld->stepSimulation(1.f / 60.f, 2);
+		dynamicsWorld->stepSimulation(frameTime * 0.001f, 2);
 		
+		// physics 
 		//print positions of all objects
 		for (int j = dynamicsWorld->getNumCollisionObjects() - 1; j >= 0; j--)
 		{
@@ -227,18 +217,40 @@ int main(int argc, char **argv) {
 			{
 				trans = obj->getWorldTransform();
 			}
-			//printf("world pos object %d = %f,%f,%f\n", j, float(trans.getOrigin().getX()), float(trans.getOrigin().getY()), float(trans.getOrigin().getZ()));
+			printf("world pos object %d = %f,%f,%f\n", j, float(trans.getOrigin().getX()), float(trans.getOrigin().getY()), float(trans.getOrigin().getZ()));
+		}
+		
+		// draw
+		
+		GX_SetViewport(0,0,rmode->fbWidth,rmode->efbHeight,0,1);
+		GX_InvVtxCache();
+		GX_InvalidateTexAll();
+		
+		GX_SetZMode(GX_TRUE, GX_LEQUAL, GX_TRUE);
+		GX_SetColorUpdate(GX_TRUE);
+		GX_CopyDisp(xfb[currentBuffer],GX_TRUE);
+		
+		GX_DrawDone();
+		
+		VIDEO_SetNextFramebuffer(xfb[currentBuffer]);
+		VIDEO_Flush();
+		
+		float fps = 1.0f / (frameTime * 0.001f);
+		
+		int buttonsDown = PAD_ButtonsDown(0);
+		
+		//if( buttonsDown & PAD_BUTTON_A ) {
+		//	printf("Button A pressed.\n");
+		//}
+		//
+		//if( buttonsDown & PAD_BUTTON_B ) {
+		//	printf("fps = %.2f\n", fps);
+		//}
+
+		if (buttonsDown & PAD_BUTTON_START) {
+			break;
 		}
 	}
-	
-	avgfps /= 75.0f;
-	
-	printf("average fps during latter half of physics sim = %.2f\n", avgfps);
-	printf("last fps value = %.2f\n", fps);
-	
-	///-----stepsimulation_end-----
-	
-	//cleanup in the reverse order of creation/initialization
 	
 	///-----cleanup_start-----
 	
@@ -280,56 +292,66 @@ int main(int argc, char **argv) {
 	//next line is optional: it will be cleared by the destructor when the array goes out of scope
 	collisionShapes.clear();
 
-	prevFrameTime = tGetTicker();
-
-	while(1) {
-
-		VIDEO_WaitVSync();
-		PAD_ScanPads();
-		
-		unsigned int now = tGetTicker();
-		float frameTime = tGetTickerDifference(prevFrameTime, now);
-		prevFrameTime = now;
-		
-		float fps = 1.0f / (frameTime * 0.001f);
-		
-		int buttonsDown = PAD_ButtonsDown(0);
-		
-		if( buttonsDown & PAD_BUTTON_A ) {
-			printf("Button A pressed.\n");
-		}
-		
-		if( buttonsDown & PAD_BUTTON_B ) {
-			printf("fps = %.2f\n", fps);
-		}
-
-		if (buttonsDown & PAD_BUTTON_START) {
-			exit(0);
-		}
-	}
-
 	return 0;
 }
 
-void * Initialise() {
-
-	void *framebuffer;
-	
+void Initialise() {
 	VIDEO_Init();
 	PAD_Init();
 	
 	rmode = VIDEO_GetPreferredMode(NULL);
 
-	framebuffer = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));
-	console_init(framebuffer,20,20,rmode->fbWidth,rmode->xfbHeight,rmode->fbWidth*VI_DISPLAY_PIX_SZ);
+	void *gp_fifo = NULL;
+	gp_fifo = memalign(32,GX_FIFO_MINSIZE);
+	memset(gp_fifo,0,GX_FIFO_MINSIZE);
+	
+	xfb[0] = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));
+	xfb[1] = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));
+	console_init(xfb[currentBuffer],20,20,rmode->fbWidth,rmode->xfbHeight,rmode->fbWidth*VI_DISPLAY_PIX_SZ);
 	
 	VIDEO_Configure(rmode);
-	VIDEO_SetNextFramebuffer(framebuffer);
+	VIDEO_SetNextFramebuffer(xfb[currentBuffer]);
 	VIDEO_SetBlack(FALSE);
 	VIDEO_Flush();
 	VIDEO_WaitVSync();
 	if(rmode->viTVMode&VI_NON_INTERLACE) VIDEO_WaitVSync();
-
-	return framebuffer;
-
+	
+	// initialize GX
+	GX_Init(gp_fifo, GX_FIFO_MINSIZE);
+	
+	// clear
+	GX_SetCopyClear(background, 0x00ffffff);
+	
+	
+	GX_SetViewport(0,0,rmode->fbWidth,rmode->efbHeight,0,1);
+	yscale = GX_GetYScaleFactor(rmode->efbHeight,rmode->xfbHeight);
+	xfbHeight = GX_SetDispCopyYScale(yscale);
+	GX_SetScissor(0,0,rmode->fbWidth,rmode->efbHeight);
+	GX_SetDispCopySrc(0,0,rmode->fbWidth,rmode->efbHeight);
+	GX_SetDispCopyDst(rmode->fbWidth,xfbHeight);
+	GX_SetCopyFilter(rmode->aa,rmode->sample_pattern,GX_TRUE,rmode->vfilter);
+	GX_SetFieldMode(rmode->field_rendering,((rmode->viHeight==2*rmode->xfbHeight)?GX_ENABLE:GX_DISABLE));
+ 
+	// cull none because other values produce weird results
+	GX_SetCullMode(GX_CULL_NONE);
+	GX_CopyDisp(xfb[currentBuffer],GX_TRUE);
+	GX_SetDispCopyGamma(GX_GM_1_0);
+ 
+	// setup our camera at the origin
+	// looking down the -z axis with y up
+	guVector cam = {0.0F, 0.0F, 0.0F},
+			up = {0.0F, 1.0F, 0.0F},
+		  look = {0.0F, 0.0F, -1.0F};
+	guLookAt(v, &cam, &up, &look);
+ 
+	// setup our projection matrix
+	// this creates a perspective matrix with a view angle of 60,
+	// an aspect ratio of 4/3 (i'm not sure if that's the right
+	// way to do it but i just went by what made a square on my screen)
+	// and z near and far distances
+    f32 w = rmode->viWidth;
+    f32 h = rmode->viHeight;
+	guPerspective(p, 60, (f32)w/h, 10.0F, 300.0F);
+	GX_LoadProjectionMtx(p, GX_PERSPECTIVE);
+	
 }
