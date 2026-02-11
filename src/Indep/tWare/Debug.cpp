ea547a2bc657ff91d55ff4ec8243f0a077428136
Dorian Pazur <c00305750@setu.ie>
2026-02-11 13:52:32 +0000

    forgot about the wii

--- src/Indep/tWare/Debug.cpp
+++ src/Indep/tWare/Debug.cpp
@@ -73,7 +73,7 @@ void tBreak()
 {
 #if defined(EA_COMPILER_MSVC) && defined(EA_PLATFORM_WINDOWS)
 	__debugbreak();
-#elif defined(EA_PLATFORM_GAMECUBE)
+#elif defined(EA_PLATFORM_GAMECUBE) || defined(EA_PLATFORM_REVOLUTION)
 	OSPanic(filename, linenum, "Breakpoint hit");
 #else
 	*(uint8_t*)0 = 0; // trigger access violation as a last resort
