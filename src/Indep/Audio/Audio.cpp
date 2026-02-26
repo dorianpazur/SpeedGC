4480466e41a4ed4962d88ea80c1643afebc7882e
Dorian Pazur <c00305750@setu.ie>
2026-02-26 12:39:11 +0000

    reading wav files instead of raw

--- src/Indep/Audio/Audio.cpp
+++ src/Indep/Audio/Audio.cpp
@@ -11,7 +11,212 @@
 
 //---------------------------------------------------------------------------------
 
-uint64_t dspTime = 0;
+void EndianSwap(int16_t& i)
+{
+	uint16_t* data = (uint16_t*)&i;
+	uint16_t swap = (*data >> 8) | (*data << 8);
+	i = *(int16_t*)&swap;
+}
+
+void EndianSwap(uint16_t& i)
+{
+	uint16_t* data = (uint16_t*)&i;
+	uint16_t swap = (*data >> 8) | (*data << 8);
+	i = *(uint16_t*)&swap;
+}
+
+void EndianSwap(int32_t& i)
+{
+	uint32_t* data = (uint32_t*)&i;
+	uint32_t swap =	((*data & 0xFF000000) >> 24) |
+					((*data & 0x00FF0000) >> 8) |
+					((*data & 0x0000FF00) << 8) |
+					((*data & 0x000000FF) << 24);
+	
+	i = *(int32_t*)&swap;
+}
+
+void EndianSwap(uint32_t& i)
+{
+	uint32_t* data = (uint32_t*)&i;
+	uint32_t swap =	((*data & 0xFF000000) >> 24) |
+					((*data & 0x00FF0000) >> 8) |
+					((*data & 0x0000FF00) << 8) |
+					((*data & 0x000000FF) << 24);
+	
+	i = *(uint32_t*)&swap;
+}
+
+int16_t EndianSwapSample(int16_t i)
+{
+	uint16_t* data = (uint16_t*)&i;
+	uint16_t swap = (*data >> 8) | (*data << 8);
+	return *(int16_t*)&swap;
+}
+
+//---------------------------------------------------------------------------------
+
+struct WaveHeader
+{
+	struct RIFFChunk
+	{
+		// RIFF header
+		
+		uint32_t ChunkID;
+		uint32_t ChunkSize;
+		uint32_t ChunkFmt;
+		
+		void EndianSwapData()
+		{
+			EndianSwap(ChunkID);
+			EndianSwap(ChunkSize);
+			EndianSwap(ChunkFmt);
+		}
+	};
+	
+	struct FmtChunk
+	{
+		// RIFF header
+		uint32_t ChunkID;
+		uint32_t ChunkSize;
+		uint16_t AudioFormat;
+		uint16_t NumChannels;
+		uint32_t SampleRate;
+		uint32_t ByteRate;
+		uint16_t BlockAlign;
+		uint16_t BitsPerSample;
+		
+		void EndianSwapData()
+		{
+			EndianSwap(ChunkID);
+			EndianSwap(ChunkSize);
+			EndianSwap(AudioFormat);
+			EndianSwap(NumChannels);
+			EndianSwap(SampleRate);
+			EndianSwap(ByteRate);
+			EndianSwap(BlockAlign);
+			EndianSwap(BitsPerSample);
+		}
+	};
+	
+	struct DataChunk
+	{
+		// RIFF header
+		uint32_t ChunkID;
+		uint32_t ChunkSize;
+		
+		void EndianSwapData()
+		{
+			EndianSwap(ChunkID);
+			EndianSwap(ChunkSize);
+		}
+	};
+	
+	RIFFChunk RIFF;
+	FmtChunk fmt;
+	DataChunk data;
+	
+	void EndianSwapData()
+	{
+		RIFF.EndianSwapData();
+		fmt.EndianSwapData();
+		data.EndianSwapData();
+	}
+};
+
+struct AudioStream
+{
+	FILE* file;
+	
+	WaveHeader header;
+	double dspTime = 0;
+	size_t size = 0;
+	bool playing = false;
+	
+	#define AudioStreamAssert(cond) \
+	if (!(cond)) \
+	{ \
+		printf("AudioStreamAssert failed on line %u: %s was false\n", __LINE__, #cond); \
+		fclose(file); \
+		gFileIOMutex.Unlock(); \
+		return false; \
+	}
+	
+	bool OpenFile(const char* path)
+	{
+		char realPath[TFILE_MAX_PATH + 1];
+	
+		snprintf(realPath, TFILE_MAX_PATH, "%s%s", gBaseDir, path);
+		
+		gFileIOMutex.Lock();
+		file = fopen(realPath, "rb");
+		
+		if (!file)
+		{
+			gFileIOMutex.Unlock();
+			return false;
+		}
+		
+		fseek(file, 0, SEEK_END);
+		size = ftell(file);
+		fseek(file, 0, SEEK_SET);
+		
+		AudioStreamAssert(size > sizeof(header)); // definitely not a wave file if smaller than header
+		
+		fread(&header, sizeof(header), 1, file); // read header
+		
+		header.EndianSwapData(); // endian swap the data within
+		
+		// check header if it's valid
+		
+		AudioStreamAssert(header.RIFF.ChunkID == 0x46464952); // check if says RIFF
+		AudioStreamAssert(header.RIFF.ChunkFmt == 0x45564157); // check if says WAVE
+		
+		AudioStreamAssert(header.fmt.ChunkID == 0x20746D66); // check if says WAVE
+		AudioStreamAssert(header.fmt.AudioFormat == 1); // check if wave is PCM
+		
+		AudioStreamAssert(header.data.ChunkID == 0x61746164); // check if says data	
+		
+		gFileIOMutex.Unlock();
+		playing = true;
+		printf("Now Playing: %s\n", path);
+		return true;
+	};
+	
+	void GetData(void* dest, size_t bufferSize)
+	{
+		if (file)
+		{	
+			gFileIOMutex.Lock();
+			
+			if (((size_t)ftell(file)) > size - bufferSize)
+				fseek(file, sizeof(header), SEEK_SET); // go back to start of audio buffer to loop
+			
+			fread(dest, bufferSize, 1, file);
+			
+			gFileIOMutex.Unlock();
+		}
+	}
+	
+	void CloseFile()
+	{
+		gFileIOMutex.Lock();
+		if (file)
+			fclose(file);
+		gFileIOMutex.Unlock();
+		playing = false;
+		file = NULL;
+	};
+	
+	~AudioStream()
+	{
+		CloseFile();
+	};
+	
+	#undef AudioStreamAssert
+};
+
+AudioStream stream;
 
 static lwpq_t thQueue;
 static lwp_t thread_handle;
@@ -28,59 +233,25 @@ tMutex gAudioMutexBuf1;
 tMutex gAudioMutexBuf2;
 tMutex gDSPTimeMutex;
 
-inline int16_t EndianSwapI16(int16_t i)
-{
-	uint16_t* data = (uint16_t*)&i;
-	uint16_t swap = (*data >> 8) | (*data << 8);
-	return *(int16_t*)&swap;
-}
-
 void UpdateAudio()
 {
 	static uint64_t prevFrame = -1;
 	static uint64_t fileOffset = 0;
 	
-	char path[TFILE_MAX_PATH + 1];
-	
-	snprintf(path, TFILE_MAX_PATH, "%s%s", gBaseDir, "title.raw");
-	
 	gDSPTimeMutex.Lock();
-	uint64_t curFrame = 1 - (dspTime / kNumSamplesInBuffer); // get the other frame
+	uint64_t curFrame = 1 - ((uint64_t)stream.dspTime / kNumSamplesInBuffer); // get the other frame
 	gDSPTimeMutex.Unlock();
 	
 	if (prevFrame != curFrame)
 	{
 		tMutex& mutex = curFrame ? gAudioMutexBuf2 : gAudioMutexBuf1;
 		
-		gFileIOMutex.Lock();
-		
-		static FILE* audioFile = fopen(path, "rb");
-		
-		if (audioFile)
+		if (stream.playing)
 		{
-			static long size = -1;
-			if (size == -1)
-			{
-				fseek(audioFile, 0, SEEK_END);
-				size = ftell(audioFile);
-				fseek(audioFile, 0, SEEK_SET);
-			}
-			
-			if (ftell(audioFile) > size - kBufferSize)
-				fseek(audioFile, 0, SEEK_SET);
-			
 			mutex.Lock();
-			
-			fread(streamingBuffer[curFrame], kBufferSize, 1, audioFile);
-			
+			stream.GetData(streamingBuffer[curFrame], kBufferSize);
 			mutex.Unlock();
-			
-			fileOffset = ftell(audioFile);
 		}
-	
-		//fclose(audioFile);
-		gFileIOMutex.Unlock();
-		//audioFile = NULL;
 		
 		prevFrame = curFrame;
 	}
@@ -115,20 +286,20 @@ void AudioFrameCallback()
 {
 	static bool other = true;
 	
-	if (streamingBuffer[0] && streamingBuffer[1])
+	if (streamingBuffer[0] && streamingBuffer[1] && stream.playing)
 	{
 		uint32_t currentBuffer;
 		uint32_t currentSample;
 		
 		for (int i = 0; i < 160; i++)
 		{
-			currentBuffer = (dspTime) / kNumSamplesInBuffer;
-			currentSample = (dspTime) % kNumSamplesInBuffer;
+			currentBuffer = ((uint64_t)stream.dspTime) / kNumSamplesInBuffer;
+			currentSample = ((uint64_t)stream.dspTime) % kNumSamplesInBuffer;
 			
-			bufL[other ? 0 : 1][i] = EndianSwapI16(streamingBuffer[currentBuffer][(currentSample * kNumChannels) + 0]);
-			bufR[other ? 0 : 1][i] = EndianSwapI16(streamingBuffer[currentBuffer][(currentSample * kNumChannels) + 1]);
+			bufL[other ? 0 : 1][i] = EndianSwapSample(streamingBuffer[currentBuffer][(currentSample * stream.header.fmt.NumChannels) + 0]);
+			bufR[other ? 0 : 1][i] = EndianSwapSample(streamingBuffer[currentBuffer][(currentSample * stream.header.fmt.NumChannels) + 1]);
 			
-			dspTime = (dspTime + 1) % (kNumSamplesInBuffer * 2); // both buffers
+			stream.dspTime = fmod(stream.dspTime + (stream.header.fmt.SampleRate / 32000.0), (kNumSamplesInBuffer * 2)); // both buffers
 		}
 		
 		DCFlushRange(bufL[other ? 0 : 1], 160 * sizeof(int32_t));
@@ -176,8 +347,8 @@ namespace Audio
 		
 		LWP_InitQueue(&thQueue);
 		LWP_CreateThread(&thread_handle, AudioThread, NULL, &gAudioStack, kAudioStackSize, 0);
-
-		play = true;
+		
+		stream.OpenFile("Audio/Music/title.wav");
 	}
 	
 	void Update()
