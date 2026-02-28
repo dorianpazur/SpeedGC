ed65174880422b23901b807176c3d5e8abd32ac1
Zaid Elkhazendar <c00314875@setu.ie>
2026-02-28 23:01:46 +0000

    Battery model file path updated

--- src/Indep/Misc/main.cpp
+++ src/Indep/Misc/main.cpp
@@ -271,7 +271,7 @@ void LoadAssets()
 	
 	gCarModel = new vModel("Vehicles/126p/126p.glb");
 
-	gBatteryModel = new vModel("Indep/sphere.glb");
+	gBatteryModel = new vModel("sphere.glb");
 	if (gBatteryModel && gBatteryModel->IsEmpty())
 	{
 		delete gBatteryModel;
